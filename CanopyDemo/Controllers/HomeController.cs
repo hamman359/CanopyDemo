c2f8e7723a6c2e6219cc0292497b6bb95e88d0c0
Jonathan Hammond <hamman359@gmail.com>
2014-06-17 20:11:09 -0400

    Initial Commit

---
+++ CanopyDemo/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CanopyDemo.Models;
+
+namespace CanopyDemo.Controllers {
+
+	public class HomeController : Controller {
+
+		private Dictionary<string, string> stateList = new Dictionary<string, string>()
+			{
+				{"", "--Select--"},
+				{"AL","Alabama"},
+				{"AK","Alaska"},
+				{"AZ","Arizona"},
+				{"AR","Arkansas"},
+				{"CA","California"},
+				{"CO","Colorado"},
+				{"CT","Connecticut"},
+				{"DE","Delaware"},
+				{"FL","Florida"},
+				{"GA","Georgia"},
+				{"HI","Hawaii"},
+				{"ID","Idaho"},
+				{"IL","Illinois"},
+				{"IN","Indiana"},
+				{"IA","Iowa"},
+				{"KS","Kansas"},
+				{"KY","Kentucky"},
+				{"LA","Louisiana"},
+				{"ME","Maine"},
+				{"MD","Maryland"},
+				{"MA","Massachusetts"},
+				{"MI","Michigan"},
+				{"MN","Minnesota"},
+				{"MS","Mississippi"},
+				{"MO","Missouri"},
+				{"MT","Montana"},
+				{"NE","Nebraska"},
+				{"NV","Nevada"},
+				{"NH","New Hampshire"},
+				{"NJ","New Jersey"},
+				{"NM","New Mexico"},
+				{"NY","New York"},
+				{"NC","North Carolina"},
+				{"ND","North Dakota"},
+				{"OH","Ohio"},
+				{"OK","Oklahoma"},
+				{"OR","Oregon"},
+				{"PA","Pennsylvania"},
+				{"RI","Rhode Island"},
+				{"SC","South Carolina"},
+				{"SD","South Dakota"},
+				{"TN","Tennessee"},
+				{"TX","Texas"},
+				{"UT","Utah"},
+				{"VT","Vermont"},
+				{"VA","Virginia"},
+				{"WA","Washington"},
+				{"WV","West Virginia"},
+				{"WI","Wisconsin"},
+				{"WY","Wyoming"},
+				{"DC","District of Columbia"}
+			};
+
+		[HttpGet]
+		public ActionResult Index() {
+
+			ViewBag.States = stateList;
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Index(Person model) {
+
+			return View("Results", model);
+		}
+
+		[HttpGet]
+		public ActionResult Results(Person model) {
+
+			return View("Results", model);
+		}
+	}
+}
---
+++ FSharpTests/Program.fs
@@ -0,0 +1,46 @@
+﻿//#r "canopy.dll"
+
+open canopy
+open runner
+open System
+
+open configuration
+open reporters
+
+reporter <- new LiveHtmlReporter() :> IReporter
+
+start firefox
+let mainBrowser = browser
+start chrome
+let secondBrowser = browser
+//start ie
+
+tile browsers
+
+switchTo mainBrowser
+
+"Demo Test" &&& fun _ ->
+
+    url "http://localhost:54008/home/demo"
+
+    "#firstName" << "John"
+    "#lastName" << "Doe"
+
+    "#firstName" == "John"
+    "#lastName" == "Smith"
+
+    
+// run the tests
+run()
+
+switchTo secondBrowser
+
+run()
+
+quit mainBrowser
+quit secondBrowser
+
+printfn "press [enter] to exit"
+System.Console.ReadLine() |> ignore
+
+quit()
